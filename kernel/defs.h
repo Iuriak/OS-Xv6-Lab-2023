b52caf69907705db971ba8325794a589f53722ea
Iuriak <reveriebrave@163.com>
2023-08-05 17:32:41 +0800

    Detecting which pages have been accessed

--- kernel/defs.h
+++ kernel/defs.h
@@ -171,6 +171,7 @@ int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 void            vmprint(pagetable_t);
+pte_t *         walk(pagetable_t, uint64, int);
 
 // plic.c
 void            plicinit(void);
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -81,6 +81,31 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 va;        // 用于存储用户提供的虚拟地址
+  int pagenum;      // 用于存储要检查的页数
+  uint64 abitsaddr; // 用于存储位掩码的用户空间缓冲区地址
+  argaddr(0, &va);        // 解析第一个参数，即虚拟地址
+  argint(1, &pagenum);    // 解析第二个参数，即要检查的页数
+  argaddr(2, &abitsaddr); // 解析第三个参数，即位掩码缓冲区地址
+
+  uint64 maskbits = 0; // 存储位掩码的临时变量
+  struct proc *proc = myproc(); // 获取当前进程
+  // 遍历指定的页数
+  for (int i = 0; i < pagenum; i++) {
+    pte_t *pte = walk(proc->pagetable, va+i*PGSIZE, 0);
+    if (pte == 0)
+      panic("page not exist.");
+  
+    if (PTE_FLAGS(*pte) & PTE_A) {
+      maskbits = maskbits | (1L << i);
+    }
+    // clear PTE_A, set PTE_A bits zero
+    *pte = ((*pte&PTE_A) ^ *pte) ^ 0 ;
+  }
+  // 将位掩码复制到用户空间
+  if (copyout(proc->pagetable, abitsaddr, (char *)&maskbits, sizeof(maskbits)) < 0)
+    panic("sys_pgacess copyout error");
+
   return 0;
 }
 #endif
