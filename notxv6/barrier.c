7a56f7ada5df32d761b4438e8f91c0b6a3efdf6e
Iuriak <reveriebrave@163.com>
2023-08-07 14:44:04 +0800

    Lab 6.3 Barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -6,6 +6,7 @@
 
 static int nthread = 1;
 static int round = 0;
+pthread_mutex_t lock;            // declare a lock
 
 struct barrier {
   pthread_mutex_t barrier_mutex;
@@ -22,6 +23,12 @@ barrier_init(void)
   bstate.nthread = 0;
 }
 
+static void barrier_destroy(void) {
+  assert(pthread_mutex_destroy(&bstate.barrier_mutex) == 0);
+  assert(pthread_cond_destroy(&bstate.barrier_cond) == 0);
+}
+
+
 static void 
 barrier()
 {
@@ -30,14 +37,30 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
+  // Lock the barrier's mutex to ensure exclusive access
+  pthread_mutex_lock(&bstate.barrier_mutex);
   
+  if (++bstate.nthread < nthread) {
+    // If not all threads have reached the barrier yet
+    // Wait on the condition variable, releasing the lock
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  } else {
+    // When all threads have reached the barrier
+    // Reset the counter for the next round, increment the round, and signal all waiting threads
+    bstate.nthread = 0;
+    ++bstate.round;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  
+  // Release the lock on the barrier's mutex
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
 thread(void *xa)
 {
-  long n = (long) xa;
-  long delay;
+  //long n = (long) xa;
+  //long delay;
   int i;
 
   for (i = 0; i < 20000; i++) {
@@ -56,7 +79,8 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   long i;
-  double t1, t0;
+  //double t1, t0;
+  pthread_mutex_init(&lock, NULL); // initialize the lock
 
   if (argc < 2) {
     fprintf(stderr, "%s: %s nthread\n", argv[0], argv[0]);
@@ -74,5 +98,8 @@ main(int argc, char *argv[])
   for(i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
+
+  barrier_destroy();
+
   printf("OK; passed\n");
 }
